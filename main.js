f8e872f24e820389fce3dda9ec166e452e27224b
christian <christiancasey969@gmail.com>
2016-07-09 12:22:17 +0100

    object array added for collection of images sounds

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 console.log('JS Loaded!');
 
 // Play game
-// 3 questions
+// (3) questions
 // click the start button to play audio clip (each clip is 10 sec approx)
-// player chooses which image for the audio clip
-// image then (flips) to display correct / wrong answer (counter shows no of questions)
-// this continues until 3 questions to win, if wrong answer the player loses, game stops. 
+// player chooses which image of 3 for the 1 audio clip.
+// image chosen then (flips) to display correct / wrong answer (counter shows no of questions)
+// this continues until (3) questions to win, if wrong answer the player loses, game stops. 
 
 // there is a progress bar to display time left to answer
 // there is potentially a level 2. 
@@ -14,16 +14,53 @@ document.getElementById("start").addEventListener("click", function(e){
   alert("Get Ready!");
   var round = 0;
   var rounds = [{
-    images: ["images/breakingbad.png", "images/narcos.png"],
-    sounds: ["breakbadsound.mp3", "narcossound.mp3"]
-  },
-  {
+    images: ["images/houseTV.jpg", "images/prisonTV.jpg", "images/frasierTV.jpg"],
+    sounds: ["House.mp3"]
+  }];
 
+  var roundsTwo = [{
+    images: ["images/veepTV.jpg", "images/cardsTV.jpg", "images/dexterTV.jpg"],
+    sounds: ["Dexter.mp3"]
+  }];
+
+  var roundsThree = [{
+    images: ["images/dangermouseTV.jpg", "images/policeTV.jpg", "images/mashTV.jpg"],
+    sounds: ["Police_Squad.mp3"]
+
+
+// document.getElementById('image1').addEventListener('click', function() {
+
+// });
+
+// document.getElementById('image2').addEventListener('click', function() {
+
+// });
+  
+// document.getElementById('image3').addEventListener('click', function() {
+
+// });
+
+  if (rounds.images === "images/frasierTV.jpg") {
+    alert('Wrong answer!');
+    // flip the card
   }
-  ];
+  else if (rounds.images === "images/prisonTV.jpg") {
+    alert('Wrong answer!');
+    // flip the card
+  }
+  else { rounds.images === "images/houseTV.jpg" 
+      alert('Correct answer!');
+    // flip the card
+  }    
 
-  // var images = ["images/breakingbad.png", "images/narcos.png"];
-  // var sounds = ["breakbadsound.mp3", "narcossound.mp3"];
+
+
+  //if(rounds.images === 0) {
+
+  //}
+
+  }];
+  
   // if (round === 1){
   //   images[round]
   //   sounds[round]
@@ -31,21 +68,13 @@ document.getElementById("start").addEventListener("click", function(e){
   // }
 
 
-  // add audio to click start button
-  var questionOne = new Audio("audio/" + "House" + ".mp3");   
-    
-  questionOne.play(); 
-
-
-
-
-
-  if () {
-
-  }
-
-
-
+  // // add audio to click start button
+  // var questionOne = new Audio("audio/" + "House" + ".mp3");   
+  //   questionOne.play(); 
 
 
 });
+
+
+
+
