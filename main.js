8c90bd03208a6296312c71439427705d3c93ebfa
christian <christiancasey969@gmail.com>
2016-07-09 13:00:57 +0100

    images added via JavaScript

--- main.js
+++ main.js
@@ -8,58 +8,59 @@ console.log('JS Loaded!');
 // this continues until (3) questions to win, if wrong answer the player loses, game stops. 
 
 // there is a progress bar to display time left to answer
-// there is potentially a level 2. 
+// there is potentially a level 2.
+
+var level = 0;
+var rounds = [{
+  images: ["images/houseTV.jpg", "images/prisonTV.jpg", "images/frasierTV.jpg"],
+  sound: "House.mp3"
+}, {
+  images: ["images/veepTV.jpg", "images/cardsTV.jpg", "images/dexterTV.jpg"],
+  sound: "Dexter.mp3"
+}, {
+  images: ["images/dangermouseTV.jpg", "images/policeTV.jpg", "images/mashTV.jpg"],
+  sound: "Police.mp3"
+}];
+
+var images = document.getElementsByTagName("img");
 
 document.getElementById("start").addEventListener("click", function(e){
   alert("Get Ready!");
-  var round = 0;
-  var rounds = [{
-    images: ["images/houseTV.jpg", "images/prisonTV.jpg", "images/frasierTV.jpg"],
-    sounds: ["House.mp3"]
-  }];
 
-  var roundsTwo = [{
-    images: ["images/veepTV.jpg", "images/cardsTV.jpg", "images/dexterTV.jpg"],
-    sounds: ["Dexter.mp3"]
-  }];
+  rounds[level].images.forEach(function(imgSrc, i) {
+    images[i].setAttribute('src', imgSrc);
+  });
 
-  var roundsThree = [{
-    images: ["images/dangermouseTV.jpg", "images/policeTV.jpg", "images/mashTV.jpg"],
-    sounds: ["Police_Squad.mp3"]
+  var audio = new Audio("audio/" + rounds[level].sound);
+  audio.play();
 
+});
 
-// document.getElementById('image1').addEventListener('click', function() {
+// add event listeners to all images (using a loop)
 
-// });
+// check image src of the clicked image against the name of the current audio
+// this.getAttribute('src') --> "images/prisonTV"
+// rounds[level].sound
+// check they match
 
-// document.getElementById('image2').addEventListener('click', function() {
-
-// });
-  
-// document.getElementById('image3').addEventListener('click', function() {
-
-// });
-
-  if (rounds.images === "images/frasierTV.jpg") {
-    alert('Wrong answer!');
-    // flip the card
-  }
-  else if (rounds.images === "images/prisonTV.jpg") {
-    alert('Wrong answer!');
-    // flip the card
-  }
-  else { rounds.images === "images/houseTV.jpg" 
-      alert('Correct answer!');
-    // flip the card
-  }    
+// if they match, increment score and/or round...
 
 
 
-  //if(rounds.images === 0) {
 
-  //}
 
-  }];
+
+
+
+
+
+
+
+
+
+
+
+
   
   // if (round === 1){
   //   images[round]
@@ -73,7 +74,7 @@ document.getElementById("start").addEventListener("click", function(e){
   //   questionOne.play(); 
 
 
-});
+// });
 
 
 
